28d62514a472be91666a7ebb0dfd599792832e0e
Ayu Sudi <ayusudi.abc@gmail.com>
2019-10-10 17:27:01 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <div v-if="!isLogin">
+    <Landing></Landing>
+  </div>
+  <div v-else>
+  </div>
+</div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  data() {
+    return {
+      message: 'Hello world',
+      isLogin: false,
+      page : 'landing'
+    }
+  },
+  components: {
+    Landing
+  }
+}
+// #49ACE9
+// #B62F8A
+// #F2D154
+// balck
+</script>
+
+<style>
+
+</style>
