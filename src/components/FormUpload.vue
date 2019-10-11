a9ca5c5cd86bf8e9a2bc5ebe94da63a7b3579b53
maulanaghozii <ghozihz@gmail.com>
2019-10-11 12:44:54 +0700

    modify card

---
+++ src/components/FormUpload.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form enctype="multipart/form-data">
+      <input type="file" ref="image" accept="image/*" @change="handleimage" required />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      image: null
+    };
+  }
+};
+</script>
+
+<style>
+</style>
